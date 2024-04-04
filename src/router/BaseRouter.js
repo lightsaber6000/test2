459abedc63b03af05c363a7352644e8afedd0f66
Алексей Семенов <a.semenov@nuzhnapomosh.ru>
2024-04-04 16:43:54 +0500

    fix(router): убрал неиспользуемый код

--- src/router/BaseRouter.js
+++ src/router/BaseRouter.js
@@ -5,17 +5,11 @@ export default [
         path: '/:catchAll(.*)',
         name: '404',
         component: () => import('@/views/404.vue'),
-        meta: {
-            title: '404',
-        },
     },
     {
         path: '/auth',
         name: 'auth',
         component: () => import('@/views/AuthView.vue'),
-        meta: {
-            title: 'Главн',
-        },
         beforeEnter: (to, from, next) => {
             if (store.getters.get_user_auth.token) {
                 next('/')
@@ -28,9 +22,6 @@ export default [
         path: '/',
         name: 'main',
         component: () => import('@/views/MainView.vue'),
-        meta: {
-            title: 'Главн',
-        },
         beforeEnter: (to, from, next) => {
             if (!store.getters.get_user_auth.token) {
                 next('/auth')
